a29a84d0e25b36aeaa8c5346767d3e64d06f86a3
OanaTudu <oana.tudu@gmail.com>
2018-03-16 16:33:54 -0700

    Update wrangleData.py

--- wrangleData.py
+++ wrangleData.py
@@ -47,3 +47,14 @@ c_surv_by_sex = by_sex.apply(c_deck_survival)
 # Print the survival rates
 print(c_surv_by_sex)
 
+# Create the Boolean Series: under10
+under10 = (titanic['age'] < 10).map({True:'under 10', False:'over 10'})
+
+# Group by under10 and compute the survival rate
+survived_mean_1 = titanic.groupby(under10)['survived'].mean()
+print(survived_mean_1)
+
+# Group by under10 and pclass and compute the survival rate
+survived_mean_2 = titanic.groupby([under10, 'pclass'])['survived'].mean()
+print(survived_mean_2)
+
