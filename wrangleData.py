c7454f75669aa8a9aaa1ee69d686158c16c1faaf
OanaTudu <oana.tudu@gmail.com>
2018-03-16 12:06:36 -0700

    WrangleTitanic

---
+++ wrangleData.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#Load data
+titanic = pd.read_csv('titanic.csv')
+
+# Group titanic by 'pclass': by_class
+by_class = titanic.groupby('pclass')
+
+# Select 'age' and 'fare'
+by_class_sub = by_class[['age','fare']]
+
+# Aggregate by_class_sub by 'max' and 'median': aggregated
+aggregated = by_class_sub.agg(['max', 'median'])
+
+# Print the maximum age in each class
+print(aggregated.loc[:, ('age','max')])
+
+# Print the median fare in each class
+print(aggregated.loc[:, ('fare', 'median')])
