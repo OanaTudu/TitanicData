1eb084e1622d912ff11036166d7c193112c0bc93
OanaTudu <oana.tudu@gmail.com>
2018-03-16 16:19:17 -0700

    Update wrangleData.py

--- wrangleData.py
+++ wrangleData.py
@@ -38,4 +38,12 @@ def c_deck_survival(gr):
     c_passengers = gr['cabin'].str.startswith('C').fillna(False)
 
     return gr.loc[c_passengers, 'survived'].mean()
+# Create a groupby object using titanic over the 'sex' column: by_sex
+by_sex = titanic.groupby('sex')
+
+# Call by_sex.apply with the function c_deck_survival and print the result
+c_surv_by_sex = by_sex.apply(c_deck_survival)
+
+# Print the survival rates
+print(c_surv_by_sex)
 
