a606a8490cd4d125405669740ab15370fe2421a3
OanaTudu <oana.tudu@gmail.com>
2018-03-16 15:10:55 -0700

    Update wrangleData.py

--- wrangleData.py
+++ wrangleData.py
@@ -17,3 +17,17 @@ print(aggregated.loc[:, ('age','max')])
 
 # Print the median fare in each class
 print(aggregated.loc[:, ('fare', 'median')])
+
+
+# Create a groupby object: by_sex_class
+by_sex_class = titanic.groupby(['sex', 'pclass'])
+
+# Write a function that imputes median
+def impute_median(series):
+    return series.fillna(series.median())
+
+# Impute age and assign to titanic['age']
+titanic.age = by_sex_class['age'].transform(impute_median)
+
+# Print the output of titanic.tail(10)
+print(titanic.tail(10))
